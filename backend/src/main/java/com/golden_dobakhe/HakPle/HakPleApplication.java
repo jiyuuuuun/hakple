43fc073c503c64cee5730ce2446a6ef449f00f21
jiyuuuuun <hwangjy001@naver.com>
2025-04-08 11:23:04 +0900

    chore: 프로젝트 구조 생성

---
+++ backend/src/main/java/com/golden_dobakhe/HakPle/HakPleApplication.java
@@ -0,0 +1,13 @@
+package com.golden_dobakhe.HakPle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HakPleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HakPleApplication.class, args);
+	}
+
+}
