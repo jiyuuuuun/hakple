e57593268bc6c24cfe352b1b29be074fd1038f31
jiyuuuuun <hwangjy001@naver.com>
2025-04-09 16:32:40 +0900

    feat: 프로젝트 초기 구조 세팅

---
+++ backend/src/main/java/com/golden_dobakhe/HakPle/entity/User.java
@@ -0,0 +1,7 @@
+package com.golden_dobakhe.HakPle.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+}
---
+++ backend/src/main/java/com/golden_dobakhe/HakPle/service/UserService.java
@@ -0,0 +1,7 @@
+package com.golden_dobakhe.HakPle.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
