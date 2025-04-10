6e0d92efe72d065058e2047d9ef92bb7d0b428c2
jiyuuuuun <hwangjy001@naver.com>
2025-04-10 15:57:45 +0900

    CommentDeleteResult Enum 주석 추가

--- backend/src/main/java/com/golden_dobakhe/HakPle/domain/post/comment/CommentDeleteResult.java
+++ backend/src/main/java/com/golden_dobakhe/HakPle/domain/post/comment/CommentDeleteResult.java
@@ -1,10 +1,10 @@
 package com.golden_dobakhe.HakPle.domain.post.comment;
 
 public enum CommentDeleteResult {
-    USER_NOT_FOUND,
-    COMMENT_NOT_FOUND,
-    BOARD_NOT_FOUND,
-    UNAUTHORIZED,
-    EMPTY,
-    SUCCESS
+    USER_NOT_FOUND, //유저 찾지 못함
+    COMMENT_NOT_FOUND, //댓글 찾지 못함
+    BOARD_NOT_FOUND, //게시물 찾지 못함
+    UNAUTHORIZED, //권한 없음
+    EMPTY, //빈 문자열
+    SUCCESS //성공
 }
