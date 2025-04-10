533398a8dae9885f24ae85a4729dd16e43450974
dark2138 <k12u2138@gmail.com>
2025-04-10 22:39:40 +0900

    게시판 CRUD

--- backend/src/main/java/com/golden_dobakhe/HakPle/domain/post/post/entity/Hashtag.java
+++ backend/src/main/java/com/golden_dobakhe/HakPle/domain/post/post/entity/Hashtag.java
@@ -15,4 +15,7 @@ import lombok.experimental.SuperBuilder;
 public class Hashtag extends BaseEntity {
     @Column(length=255, unique=true, nullable=false)
     String hashtagName; // 해시태그 이름 (유니크 설정됨)
+
+    @Column(nullable = false)
+    private String academyCode; // 학원 코드로 구분
 }
---
+++ backend/src/main/java/com/golden_dobakhe/HakPle/domain/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.golden_dobakhe.HakPle.domain.user.repository;
+
+import com.golden_dobakhe.HakPle.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
