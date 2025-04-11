00294cc6a36e25eb24112c44b19f38e81e9931bb
KHE <99ant@yonsei.ac.kr>
2025-04-12 01:27:28 +0900

    Feat: 내 정보 및 학원 등록 Swagger 문서 추가

--- backend/src/main/java/com/golden_dobakhe/HakPle/domain/user/myInfo/controller/ApiV1AcademyController.java
+++ backend/src/main/java/com/golden_dobakhe/HakPle/domain/user/myInfo/controller/ApiV1AcademyController.java
@@ -1,6 +1,9 @@
 package com.golden_dobakhe.HakPle.domain.user.myInfo.controller;
 
 import com.golden_dobakhe.HakPle.domain.user.myInfo.service.AcademyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiV1AcademyController {
     private final AcademyService academyService;
 
+    @Operation(
+            summary = "학원 등록",
+            description = "사용자가 학원 코드를 입력하여 학원을 등록됩니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "학원 등록 성공"),
+            @ApiResponse(responseCode = "400", description = "유효하지 않은 학원 코드"),
+            @ApiResponse(responseCode = "404", description = "해당하는 학원을 찾을 수 없음")
+    })
     @GetMapping("/register")
     public ResponseEntity<String> registerAcademy(
             @RequestParam("userName") String userName,
