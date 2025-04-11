c7cc506333caa1c755fe185c072ae6b1925d0a20
KHE <99ant@yonsei.ac.kr>
2025-04-12 01:27:28 +0900

    Feat: 내 정보 및 학원 등록 Swagger 문서 추가

--- backend/src/main/java/com/golden_dobakhe/HakPle/domain/user/myInfo/controller/ApiV1MyInfoController.java
+++ backend/src/main/java/com/golden_dobakhe/HakPle/domain/user/myInfo/controller/ApiV1MyInfoController.java
@@ -3,6 +3,9 @@ package com.golden_dobakhe.HakPle.domain.user.myInfo.controller;
 import com.golden_dobakhe.HakPle.domain.user.myInfo.dto.MyInfoResponseDto;
 import com.golden_dobakhe.HakPle.domain.user.myInfo.dto.MyInfoUpdateRequestDto;
 import com.golden_dobakhe.HakPle.domain.user.myInfo.service.MyInfoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiV1MyInfoController {
     private final MyInfoService myInfoService;
 
+    @Operation(
+            summary = "내 정보 조회",
+            description = "사용자의 프로필 정보를 조회합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "정보 조회 성공"),
+            @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음")
+    })
     @GetMapping("/myInfo")
 //    public ResponseEntity<MyInfoRequestDto> getMyInfo(Authentication authentication) {
 //        String username = authentication.getName(); // JWT 필터가 유저네임 넣어줌
@@ -26,6 +37,16 @@ public class ApiV1MyInfoController {
         return ResponseEntity.ok(dto);
     }
 
+
+    @Operation(
+            summary = "내 정보 수정",
+            description = "사용자의 닉네임, 전화번호, 학원 정보를 수정합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "정보 수정 성공"),
+            @ApiResponse(responseCode = "400", description = "입력 값 오류"),
+            @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음")
+    })
     @PatchMapping("/myinfo/update")
 //    public ResponseEntity<String> updateMyInfo(
 //            @RequestBody MyInfoUpdateRequestDto requestDto, Authentication authentication
