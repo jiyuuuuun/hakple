de390a0660a5925cc3428036426edc8fbf52d24f
KHE <99ant@yonsei.ac.kr>
2025-04-11 11:24:08 +0900

    Feat: #24 사용자 정보 수정 API 설계

--- backend/src/main/java/com/golden_dobakhe/HakPle/domain/user/myInfo/controller/ApiV1MyInfoController.java
+++ backend/src/main/java/com/golden_dobakhe/HakPle/domain/user/myInfo/controller/ApiV1MyInfoController.java
@@ -1,10 +1,13 @@
 package com.golden_dobakhe.HakPle.domain.user.myInfo.controller;
 
-import com.golden_dobakhe.HakPle.domain.user.myInfo.dto.MyInfoRequestDto;
+import com.golden_dobakhe.HakPle.domain.user.myInfo.dto.MyInfoResponseDto;
+import com.golden_dobakhe.HakPle.domain.user.myInfo.dto.MyInfoUpdateRequestDto;
 import com.golden_dobakhe.HakPle.domain.user.myInfo.service.MyInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +21,21 @@ public class ApiV1MyInfoController {
     @GetMapping("/myInfo")
 //    public ResponseEntity<MyInfoRequestDto> getMyInfo(Authentication authentication) {
 //        String username = authentication.getName(); // JWT 필터가 유저네임 넣어줌
-    public ResponseEntity<MyInfoRequestDto> getMyInfo(@RequestParam("userName") String userName) {
-        MyInfoRequestDto dto = myInfoService.getMyInfo(userName);
+    public ResponseEntity<MyInfoResponseDto> getMyInfo(@RequestParam("userName") String userName) {
+        MyInfoResponseDto dto = myInfoService.getMyInfo(userName);
         return ResponseEntity.ok(dto);
+    }
+
+    @PatchMapping("/myinfo/update")
+//    public ResponseEntity<String> updateMyInfo(
+//            @RequestBody MyInfoUpdateRequestDto requestDto, Authentication authentication
+//    ) {
+//        String username = authentication.getName(); // JWT 필터에서 유저네임 꺼내옴
+    public ResponseEntity<String> updateMyInfo(
+            @RequestParam("userName") String userName,
+            @RequestBody MyInfoUpdateRequestDto myInfoUpdateRequestDto) {
+
+        myInfoService.updateMyInfo(userName, myInfoUpdateRequestDto);
+        return ResponseEntity.ok("회원 정보가 성공적으로 수정되었습니다.");
     }
 }
