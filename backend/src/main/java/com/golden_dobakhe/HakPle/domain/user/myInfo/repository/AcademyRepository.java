12f32ddbfcbe3f5f111c740a7b113f1163b6f5c7
KHE <99ant@yonsei.ac.kr>
2025-04-11 14:16:04 +0900

    Feat: #24 학원 등록 API 설계

---
+++ backend/src/main/java/com/golden_dobakhe/HakPle/domain/user/myInfo/repository/AcademyRepository.java
@@ -0,0 +1,9 @@
+package com.golden_dobakhe.HakPle.domain.user.myInfo.repository;
+
+import com.golden_dobakhe.HakPle.domain.user.entity.Academy;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AcademyRepository extends JpaRepository<Academy, Long> {
+    Optional<Academy> findByPhoneNumEndsWith(String suffix);
+}
