448453c9606725b9fa2153ea38f9a2497b3dd6bf
jiyuuuuun <hwangjy001@naver.com>
2025-04-11 09:35:19 +0900

    feat: Swagger 어노테이션 추가

--- backend/src/main/java/com/golden_dobakhe/HakPle/domain/post/comment/dto/CommentRequestDto.java
+++ backend/src/main/java/com/golden_dobakhe/HakPle/domain/post/comment/dto/CommentRequestDto.java
@@ -1,5 +1,6 @@
 package com.golden_dobakhe.HakPle.domain.post.comment.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,8 +8,15 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "댓글 요청 DTO")
 public class CommentRequestDto {
-    private Long boardId; //생성
-    private String content; //생성
-    private Long commenterId; //수정
+
+    @Schema(description = "게시글 ID", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Long boardId;
+
+    @Schema(description = "댓글 내용", example = "이 게시글 정말 좋네요!", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String content;
+
+    @Schema(description = "댓글 작성자 ID", example = "100", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Long commenterId;
 }
