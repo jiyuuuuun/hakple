07b111e7f1579c70b03d708b936010d3236bab9a
JAWSP <lightload2912@gmail.com>
2025-04-11 13:25:03 +0900

    login with jwt

--- backend/src/main/java/com/golden_dobakhe/HakPle/security/SecurityConfig.java
+++ backend/src/main/java/com/golden_dobakhe/HakPle/security/SecurityConfig.java
@@ -4,7 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @Slf4j
@@ -15,18 +24,47 @@ public class SecurityConfig {
         security
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/success", "/failure").permitAll())
-        ;
+                        .requestMatchers("/", "/success", "/failure", "/login").permitAll())
+                .sessionManagement(session -> session
+                        //세션을 저장하지 않는다 -> 세션을 사용하지 않겠다는 뜻 jwt인증을 쓸거니까
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                //http베이직은 헤더에서 보안에 취약하고 쟤를 빼버리고, 다른 인증수단인 베어러(얜 이거 빼면 자동으로 지정됨)으로 한다고 한다
+                //이후 요청시 헤더에 Authorization
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+
+        //security.build();
+
         //로그인
         security
-                .formLogin(form -> form
-                        .defaultSuccessUrl("/success")
-                        .failureUrl("/failure")
+                .formLogin(form -> form.disable()
                 );
 
-        //로그아웃
 
-        //세션
+
+
         return  security.build();
+    }
+
+    //cors에 대한 설정
+    //요청이 들어오면 어디까지 허용할꺼냐
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        //혀용을 어디까지 허용할꺼냐를 설정한다
+        //헤더, 메서드, 오리진 HTTP메서드...
+        config.addAllowedOrigin("*"); //origin는 naver,goole,localhost같은거
+        config.addAllowedHeader("*"); //베어러는 여기에 포함된다
+        config.addAllowedMethod("*");
+        config.setAllowedMethods(List.of("GET","POST","DELETE"));
+        //그리고 그걸 적용할 url에 내가 설정한 부분을 적용시킬거라고 한다
+        source.registerCorsConfiguration("/**",config);
+        return source;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ backend/src/main/java/com/golden_dobakhe/HakPle/security/jwt/JwtTokenizer.java
@@ -0,0 +1,63 @@
+package com.golden_dobakhe.HakPle.security.jwt;
+
+import com.golden_dobakhe.HakPle.global.entity.Status;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class JwtTokenizer {
+    private final byte[] accessSecret;
+    private final byte[] refreshSecret;
+
+    public static Long ACCESS_TOKEN_EXPIRE_COUNT=30*60*1000L; //이거 30분임 단위가 ms라서
+    public static Long REFRESH_TOKEN_EXPIRE_COUNT=7*24*60*60*1000L; //이건 일주일
+
+    //@Value로 application.yml에 있는 환경변수값을 불러와서 생성
+    public JwtTokenizer(@Value("${jwt.secretKey}") String accessSecret, @Value("{jwt.refreshKey}") String refreshSecret) {
+        this.accessSecret = accessSecret.getBytes(StandardCharsets.UTF_8);
+        this.refreshSecret = refreshSecret.getBytes(StandardCharsets.UTF_8);
+    }
+
+    //토큰 만들기
+    private String createToken(Long id, String userName, String nickName, String phoneNum, Status status
+    , Long expire, byte[] secretKey) {
+
+        //토큰에 넣을 요소를 만들기
+        Claims claims = Jwts.claims().setSubject(userName);
+        claims.put("nickName", nickName);
+        claims.put("phoneNum", phoneNum);
+        claims.put("status", status);
+
+
+        //토큰에 그 요소들을 넣기
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime()+expire))
+                .signWith(getSignKey(secretKey))
+                .compact();
+
+    }
+
+    //access, refresh토큰 각각만들기
+    public String createAccessToken(Long id, String userName, String nickName, String phoneNum, Status status) {
+        return createToken(id, userName, nickName, phoneNum, status, ACCESS_TOKEN_EXPIRE_COUNT, accessSecret);
+    }
+
+    public String createRefreshToken(Long id, String userName, String nickName, String phoneNum, Status status) {
+        return createToken(id, userName, nickName, phoneNum, status, ACCESS_TOKEN_EXPIRE_COUNT, accessSecret);
+    }
+
+    //jwt인증에 필요한 서명만들기
+    private static Key getSignKey(byte[] secretKey) {
+        return Keys.hmacShaKeyFor(secretKey);
+    }
+}
