b455b98776ca171c012d8a9ff85c77fbef9828f4
dark2138 <k12u2138@gmail.com>
2025-04-11 13:59:23 +0900

    게시판 CRUD 버그수정

--- backend/src/main/java/com/golden_dobakhe/HakPle/domain/post/post/entity/BoardReport.java
+++ backend/src/main/java/com/golden_dobakhe/HakPle/domain/post/post/entity/BoardReport.java
@@ -13,7 +13,7 @@ import lombok.experimental.SuperBuilder;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@SuperBuilder
+@Builder
 @ToString
 public class BoardReport extends BaseEntity {
     @ManyToOne(optional = false)
