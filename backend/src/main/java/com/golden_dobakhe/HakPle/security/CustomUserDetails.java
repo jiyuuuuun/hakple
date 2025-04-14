de8f3dd84836592ff446460d44c6cb95d65b1844
황지윤 <105794920+jiyuuuuun@users.noreply.github.com>
2025-04-14 16:37:35 +0900

    Create CustomUserDetails.java

---
+++ backend/src/main/java/com/golden_dobakhe/HakPle/security/CustomUserDetails.java
@@ -0,0 +1,58 @@
+package com.golden_dobakhe.HakPle.security;
+
+import com.golden_dobakhe.HakPle.domain.user.entity.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    public Long getUserId() {
+        return user.getId();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(); // 권한 없음
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword(); // 비밀번호 (암호화된 상태)
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName(); // 유저 이름
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
