f5af74f87e9b1918ccaa487d7f641d4292ee9ed7
kim myeong soo <ekzm527@naver.com>
2025-04-14 16:53:58 +0900

    SecurityConfig 패스워드 암호화 추가

--- backend/src/main/java/com/golden_dobakhe/HakPle/security/SecurityConfig.java
+++ backend/src/main/java/com/golden_dobakhe/HakPle/security/SecurityConfig.java
@@ -4,11 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @Slf4j
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
         //접근 제한
