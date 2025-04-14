a075e74a0318e04e8ad3c6572d40060a1a8f3535
jiyuuuuun <hwangjy001@naver.com>
2025-04-14 11:52:16 +0900

    feat: SecurityUtil 예비 설정

---
+++ backend/src/main/java/com/golden_dobakhe/HakPle/security/CustomUserDetails.java
@@ -0,0 +1,42 @@
+package com.golden_dobakhe.HakPle.security;
+
+import com.golden_dobakhe.HakPle.domain.user.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    public Long getUserId() {
+        return user.getId();
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+
+}
