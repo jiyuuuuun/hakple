ec8fd78ed05f4a96399cc1bfb57d7b7e6c92fb99
kim myeong soo <ekzm527@naver.com>
2025-04-15 15:04:37 +0900

    아이디 찾기추가

---
+++ backend/src/main/java/com/golden_dobakhe/HakPle/domain/user/myInfo/controller/ApiV1UsernameController.java
@@ -0,0 +1,27 @@
+package com.golden_dobakhe.HakPle.domain.user.myInfo.controller;
+
+import com.golden_dobakhe.HakPle.domain.user.myInfo.service.UsernameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/usernames")
+public class ApiV1UsernameController {
+    private final UsernameService usernameService;
+
+    //아이디 찾기
+    @GetMapping("/find-username")
+    public ResponseEntity<String> findUserName(@RequestParam String nickName, String phoneNum) {
+        try {
+            String userName = usernameService.findUserNameByPhoneNum(nickName,phoneNum);
+            return ResponseEntity.ok("회원님의 아이디는: " + userName + " 입니다.");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
