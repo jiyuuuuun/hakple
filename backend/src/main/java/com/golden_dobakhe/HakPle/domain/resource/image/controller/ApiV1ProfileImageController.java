a04ea8379d16212264ef00fb5592bc640a10ab2d
KHE <99ant@yonsei.ac.kr>
2025-04-23 10:50:33 +0900

    Feat: #140 프로필 이미지 업로드/삭제 api

--- backend/src/main/java/com/golden_dobakhe/HakPle/domain/resource/image/controller/ApiV1ProfileImageController.java
+++ backend/src/main/java/com/golden_dobakhe/HakPle/domain/resource/image/controller/ApiV1ProfileImageController.java
@@ -3,10 +3,10 @@ package com.golden_dobakhe.HakPle.domain.resource.image.controller;
 import com.golden_dobakhe.HakPle.domain.resource.image.service.ProfileImageService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,14 +24,20 @@ public class ApiV1ProfileImageController {
     @Operation(summary = "프로필 이미지 등록/수정", description = "사용자가 자신의 프로필 사진을 업로드합니다.")
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> uploadProfileImage(
-            @RequestParam("multipartFile") MultipartFile multipartFile) throws IOException {
-        return ResponseEntity.ok((profileImageService.uploadProfileImage(multipartFile)));
+            @RequestParam("multipartFile") MultipartFile multipartFile,
+            Authentication authentication
+    ) {
+        String userName = authentication.getName(); // JWT 필터에서 유저네임 꺼내옴
+        return ResponseEntity.ok((profileImageService.uploadProfileImage(userName, multipartFile)));
     }
 
     @Operation(summary = "프로필 이미지 삭제", description = "사용자의 프로필 이미지를 삭제합니다.")
     @DeleteMapping("/delete")
-    public ResponseEntity<String> deleteProfileImage(@RequestParam String fileName) {
-        profileImageService.deleteProfileImage(fileName);
-        return ResponseEntity.ok(fileName);
+    public ResponseEntity<String> deleteProfileImage(
+            Authentication authentication
+    ) {
+        String userName = authentication.getName(); // JWT 필터에서 유저네임 꺼내옴
+        profileImageService.deleteProfileImage(userName);
+        return ResponseEntity.ok("프로필 이미지가 성공적으로 삭제되었습니다.");
     }
 }
--- backend/src/main/java/com/golden_dobakhe/HakPle/domain/resource/image/service/ProfileImageService.java
+++ backend/src/main/java/com/golden_dobakhe/HakPle/domain/resource/image/service/ProfileImageService.java
@@ -1,72 +1,131 @@
 package com.golden_dobakhe.HakPle.domain.resource.image.service;
 
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.golden_dobakhe.HakPle.domain.resource.image.entity.Image;
+import com.golden_dobakhe.HakPle.domain.resource.image.repository.ImageRepository;
+import com.golden_dobakhe.HakPle.domain.user.user.entity.User;
+import com.golden_dobakhe.HakPle.domain.user.user.repository.UserRepository;
 import java.io.IOException;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ProfileImageService {
+
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
+    private final ImageRepository imageRepository;
+    private final UserRepository userRepository;
+
     private final AmazonS3 amazonS3;
 
-    public String uploadProfileImage(MultipartFile multipartFile) throws IOException {
-        String fileName = multipartFile.getOriginalFilename();
+    // 파일명을 난수화하기 위해 UUID 를 활용하여 난수를 돌린다.
+    public String createFileName(String fileName) {
+        return UUID.randomUUID().toString().concat(getFileExtension(fileName));
+    }
 
-        //파일 형식 구하기
-        String ext = fileName.split("\\.")[1];
+    //  "."의 존재 유무만 판단
+    private String getFileExtension(String fileName) {
+        try {
+            //파일 형식 구하기
+            String ext = fileName.split("\\.")[1];
 //        String ext = fileName.substring(fileName.lastIndexOf('.') + 1).toLowerCase();
 
-        String contentType = "";
-        //content type을 지정해서 올려주지 않으면 자동으로 "application/octet-stream"으로 고정이 되서 링크 클릭시 웹에서 열리는게 아니라 자동 다운이 시작됨.
-        switch (ext) {
-            case "jpeg":
-                contentType = "image/jpeg";
-                break;
-            case "png":
-                contentType = "image/png";
-                break;
-            case "txt":
-                contentType = "text/plain";
-                break;
-            case "csv":
-                contentType = "text/csv";
-                break;
+            String contentType = "";
+            //content type을 지정해서 올려주지 않으면 자동으로 "application/octet-stream"으로 고정이 되서 링크 클릭시 웹에서 열리는게 아니라 자동 다운이 시작됨.
+            switch (ext) {
+                case "jpeg":
+                    contentType = "image/jpeg";
+                    break;
+                case "png":
+                    contentType = "image/png";
+                    break;
+                case "txt":
+                    contentType = "text/plain";
+                    break;
+                case "csv":
+                    contentType = "text/csv";
+                    break;
+            }
+            return contentType;
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 형식의 파일" + fileName + ") 입니다.");
         }
+    }
+
+    @Transactional
+    public String uploadProfileImage(String userName, MultipartFile multipartFile) {
+        User user = userRepository.findByUserName(userName)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자 없음"));
+
+        // 기존 이미지가 있다면 삭제
+        Image existingImage = user.getProfileImage();
+        if (existingImage != null) {
+            deleteS3Image(existingImage.getFilePath());
+            imageRepository.delete(existingImage);
+            user.setProfileImage(null);
+        }
+
+        String fileName = createFileName(multipartFile.getOriginalFilename());
 
         try {
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentType(contentType);
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentType(fileName);
 
             //S3에 파일 업로드
-            amazonS3.putObject(new PutObjectRequest(bucket, fileName, multipartFile.getInputStream(), metadata)
+            amazonS3.putObject(new PutObjectRequest(bucket, fileName, multipartFile.getInputStream(), objectMetadata)
                     .withCannedAcl(CannedAccessControlList.PublicRead));
-        } catch (AmazonServiceException e) {
-            e.printStackTrace();
-            log.info(e.getMessage());
-        } catch (SdkClientException e) {
-            e.printStackTrace();
-            log.info(e.getMessage());
+        } catch (IOException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다.");
         }
-        return amazonS3.getUrl(bucket, fileName).toString();
+
+        //db에 새 이미지 저장
+        String profileImageUrl = amazonS3.getUrl(bucket, fileName).toString();
+        Image newImage = Image.builder()
+                .filePath(profileImageUrl)
+                .user(user)
+                .build();
+        imageRepository.save(newImage);
+        user.setProfileImage(newImage);
+        userRepository.save(user);
+
+        return profileImageUrl;
     }
 
     // 프로필 사진 삭제
-    public void deleteProfileImage(String fileName) {
-        amazonS3.deleteObject(new DeleteObjectRequest(bucket, fileName));
-        System.out.println(bucket);
+    @Transactional
+    public void deleteProfileImage(String userName) {
+        User user = userRepository.findByUserName(userName)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자 없음"));
+
+        Image existingImage = user.getProfileImage();
+        if (existingImage != null) {
+            deleteS3Image(existingImage.getFilePath());
+
+            imageRepository.delete(existingImage);
+            user.setProfileImage(null);
+            userRepository.save(user);
+        }
+    }
+
+    private void deleteS3Image(String fileName) {
+        try {
+            String key = fileName.contains(".com/") ? fileName.split(".com/")[1] : fileName;
+            amazonS3.deleteObject(bucket, key);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "S3 이미지 삭제 실패");
+        }
     }
 }
