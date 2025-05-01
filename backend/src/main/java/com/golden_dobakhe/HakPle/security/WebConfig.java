425b58168d71f3483fd0e479341e5a767e8d5fd3
dark2138 <k12u2138@gmail.com>
2025-05-01 13:31:48 +0900

    수정2

--- backend/src/main/java/com/golden_dobakhe/HakPle/security/WebConfig.java
+++ backend/src/main/java/com/golden_dobakhe/HakPle/security/WebConfig.java
@@ -47,7 +47,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(frontUrl)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS, PATCH")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
